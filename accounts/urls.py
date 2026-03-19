d3d09abd9a27c35d852ee89ee1f17415f5bb6866
ivel225 <imelmarbarros@gmail.com>
2026-03-20 02:12:01 +0800

    Revert "Initial commit: Complete Django school project with React frontend"
    This reverts commit 3e3d660c5f6624025abcba9ec93b671b92527b12.

--- accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import SchoolLoginView
-from .views_api import LoginAPIView
-
-urlpatterns = [
-    path('login/', SchoolLoginView.as_view(), name='login'),
-    path('api/login/', LoginAPIView.as_view(), name='api_login'),
-]
--- accounts/views.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.views import LoginView
-from .forms import SchoolLoginForm
-
-class SchoolLoginView(LoginView):
-    template_name = 'registration/login.html'
-    form_class = SchoolLoginForm
-    redirect_authenticated_user = True
